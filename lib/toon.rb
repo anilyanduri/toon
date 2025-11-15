b3955c150721b37e8735750ede30fe7461aa0765
Anil Yanduri <anilkumaryln@gmail.com>
2025-11-15 20:02:52 +0530

    added core Toon module and version file

---
+++ lib/toon.rb
@@ -0,0 +1,28 @@
+require_relative "toon/version"
+require_relative "toon/encoder"
+require_relative "toon/decoder"
+require_relative "extensions/active_support"
+
+module Toon
+  class Error < StandardError; end
+
+  def self.generate(obj, **opts)
+    Toon::Encoder.generate(obj, **opts)
+  end
+
+  def self.pretty_generate(obj, **opts)
+    Toon::Encoder.pretty_generate(obj, **opts)
+  end
+
+  def self.parse(str, **opts)
+    Toon::Decoder.parse(str, **opts)
+  end
+
+  def self.load(io, **opts)
+    Toon::Decoder.load(io, **opts)
+  end
+
+  def self.dump(obj, io = STDOUT, **opts)
+    Toon::Encoder.dump(obj, io, **opts)
+  end
+end
